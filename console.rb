5b5bf80fa0a26339f1de4fe9bc0fed767bc27cbd
Ruairidh Holwill <roore@hotmail.co.uk>
2019-04-16 15:37:18 +0100

    done up to creating find method

--- console.rb
+++ console.rb
@@ -1,10 +1,21 @@
 require_relative('models/property.rb')
 #:num_bedrooms, :year_built, :status, :build
 
-Propeort.delete_all()
+Property.delete_all()
 
 options1 = {'num_bedrooms' => 3, 'year_built' => 1863, 'status' => 'buy', 'build' => 'semi-detached'}
 options2 = {'num_bedrooms' => 7, 'year_built' => 1700, 'status' => 'buy', 'build' => 'detached'}
 
 property1 = Property.new(options1)
 property2 = Property.new(options2)
+
+property1.save()
+property2.save()
+
+# property1.delete()
+property2.num_bedrooms = 15
+property2.update()
+p Property.find(property1.id)
+
+
+# p Property.all()
--- models/property.rb
+++ models/property.rb
@@ -12,6 +12,16 @@ class Property
     @build = options['build']
   end
 
+  def save()
+    db = PG.connect({dbname: 'property', host: 'localhost'});
+    sql = "INSERT INTO property (num_bedrooms, year_built, status, build) VALUES ($1, $2, $3, $4) RETURNING id;"
+    values = [@num_bedrooms, @year_built, @status, @build]
+    db.prepare("save", sql)
+    properties = db.exec_prepared("save", values)
+    @id = properties[0]['id']
+    db.close()
+  end
+
   def Property.delete_all()
     db = PG.connect({dbname: 'property', host: 'localhost'});
     sql = "DELETE FROM property;"
@@ -20,7 +30,42 @@ class Property
     db.close()
   end
 
+  def delete()
+    db = PG.connect({dbname: 'property', host: 'localhost'});
+    sql = "DELETE FROM property WHERE id = $1;"
+    values = [@id]
+    db.prepare("delete", sql)
+    db.exec_prepared("delete", values)
+    db.close()
+  end
 
+  def Property.all()
+    db = PG.connect({dbname: 'property', host: 'localhost'});
+    sql = "SELECT * FROM property;"
+    property_hashes = db.exec(sql)
+    properties = property_hashes.map { |property_hash| Property.new(property_hash) }
+    db.close()
+    return properties
+  end
 
+  def update()
+    db = PG.connect({dbname: 'property', host: 'localhost'});
+    sql = "UPDATE property SET (num_bedrooms, year_built, status, build) = ($1, $2, $3, $4) WHERE id = $5;"
+    values = [@num_bedrooms, @year_built, @status, @build, @id]
+    db.prepare("update", sql)
+    db.exec_prepared("update", values)
+    db.close()
+  end
+
+  def Property.find(id)
+    db = PG.connect({dbname: 'property', host: 'localhost'});
+    sql = "SELECT * FROM property WHERE id = $1;"
+    values = [id]
+    db.prepare("find", sql)
+    find_property = db.exec_prepared("find", values)
+    return find_property[0]
+    db.close()
+
+  end
 
 end
